df441b681e10bdfeb742ec2c05925dbb58619a42
TheBlueShi <94284796+TheBlueShi@users.noreply.github.com>
2021-12-05 16:25:59 +0700

    Update config.js

--- config.js
+++ config.js
@@ -54,7 +54,7 @@ global.APIKeys = { // APIKey Here
 
 // Sticker WM
 global.packname = 'wa.me/6285713964963'
-global.author = 'Metro Bot Is The Best'
+global.author = 'MitsuhBot Botz Is The Best'
 
 global.multiplier = 39 // The higher, The harder levelup
 
